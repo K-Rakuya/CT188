27253a315a20374060598e6dcf9e3da4bebf596f
Le Minh Phuc <phucb2410743@gmail.com>
2026-02-24 17:24:30 +0700

    sua nhe

--- Phuc/Slide_Product.js
+++ Phuc/Slide_Product.js
@@ -1,43 +1,20 @@
-// Lấy danh sách các nút dịch trái phải
-const btns_prev = document.querySelectorAll(".btn-prev");
-const btns_next = document.querySelectorAll(".btn-next");
+const btn_prev = document.querySelector(".btn-prev");
+const btn_next = document.querySelector(".btn-next");
+const product_card = document.querySelectorAll(".product-card");
+const slider_track = document.querySelector(".slider-track");
+console.log(product_card.length)
 
-//Lấy các Slider track
-const slider_track_list = document.querySelectorAll(".slider-track");
 
-//Lấy độ rộng của card
-const product_card_width = document.querySelector(".product-card").offsetWidth
-
-//Lấy danh sách các sản phẩm cafe
-const cafe_list = document.querySelectorAll(".product-card-cafe");
-
-//độ dài của khung cửa sổ
-const slider_Window_width = document.querySelector(".slider-window").offsetWidth;
-
-pds_in_window = Math.floor(slider_Window_width/product_card_width)
-
-let minOffset = - (cafe_list.length - pds_in_window) * product_card_width
-let maxOffset = 0;
-let offset = 0;
-
-//Hàm dành cho nút prev
-function slide_prev(){
-    if(offset >= maxOffset) return;
-    offset += product_card_width
-    slider_track_list[0].style.transform = `translateX(${offset}px)`;
-
+function update_width(){
+    width_of_card = product_card[0].offsetWidth;
+    return width_of_card;
 }
+let current_width = update_width();
 
-//Hàm dành cho nút next
-function slide_next(){
-    if(offset <= minOffset) return;
-    offset -= product_card_width
-    slider_track_list[0].style.transform = `translateX(${offset}px)`;
-}
+window.addEventListener("resize", ()=>{
+    current_width = update_width();
+});
 
-btns_prev[0].addEventListener("click", slide_prev)
-btns_next[0].addEventListener("click", slide_next)
-
-
-
-
+btn_next.addEventListener("click", () =>{
+    slider_track.style.transform = `translateX(-${current_width}px)`;
+});
