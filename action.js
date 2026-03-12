b26c32a9c3366dcd5b6f02327713aec809c3cca2
niodaaa <nghiakun234@gmail.com>
2026-03-12 21:44:10 +0700

    up

---
+++ action.js
@@ -0,0 +1,36 @@
+let slides = document.querySelectorAll(".slide");
+let dots = document.querySelectorAll(".dot");
+let index = 0;
+
+function showSlide(i) {
+  slides.forEach((s) => s.classList.remove("active"));
+  dots.forEach((d) => d.classList.remove("active"));
+
+  slides[i].classList.add("active");
+  dots[i].classList.add("active");
+}
+
+function nextSlide() {
+  index++;
+  if (index >= slides.length) index = 0;
+  showSlide(index);
+}
+
+function prevSlide() {
+  index--;
+  if (index < 0) index = slides.length - 1;
+  showSlide(index);
+}
+
+document.querySelector(".next").onclick = nextSlide;
+document.querySelector(".prev").onclick = prevSlide;
+
+dots.forEach((dot, i) => {
+  dot.onclick = () => {
+    index = i;
+    showSlide(index);
+  };
+});
+
+showSlide(index);
+setInterval(nextSlide, 3000);
