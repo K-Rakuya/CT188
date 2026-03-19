45a85add14e4e56e02c97abea5bfa3c7fd44e684
Le Minh Phuc <phucb2410743@gmail.com>
2026-03-19 20:47:41 +0700

    hoan thanh: bo sung them phan dat hang

--- Tea/read_data_tea.js
+++ Tea/read_data_tea.js
@@ -1,5 +1,5 @@
 import {ShowProduct} from "../ShowProduct/showPD.js";
-import {isLoggedIn} from "../CheckUser/isLogged.js"
+import {add_Event_order_btn} from "../Order/order.js";
 fetch("Tea/data_tea.json")
   .then(res => res.json())
   .then(data => renderProductsTea(data))
@@ -12,10 +12,10 @@ function renderProductsTea(products) {
 
   products.forEach(p => {
     html += `
-      <article class="product-card tea-card">
+      <article id="${p.id}" class="product-card tea-card">
           <div class="container-img"><img src="${p.image}" alt="${p.name}"></div>
           <div class="info-product">
-              <h4>${p.name}</h4>
+              <h4 class="pd-name">${p.name}</h4>
               <p class="price">${p.price.toLocaleString("vi-VN")} VNĐ</p>
           </div>
           <button class="btn-order">Thêm vào giỏ</button>
@@ -23,35 +23,7 @@ function renderProductsTea(products) {
     `;
   });
   product_grid.innerHTML = html;
-  const List_of_cards = product_grid.querySelectorAll(".product-card");
-    
-  List_of_cards.forEach(card=>{
-    const btn_order = card.querySelector(".btn-order");
-    
-    btn_order.addEventListener("click", ()=>{
-      
-      if(!isLoggedIn()){
-        window.location.href = "Register/register.html";
-      }
-
-      else{
-        const name = card.querySelector("h4").textContent;
-        const price = card.querySelector("p").textContent;
-        let image = card.querySelector("img").getAttribute("src");       
-
-        const product = {
-          name: name,
-          price: price,
-          image: image
-        }
-        
-        let list_products = JSON.parse(localStorage.getItem('products')) || [];
-        list_products.push(product);
-        localStorage.setItem("products", JSON.stringify(list_products));
-      }
-    })
-
-  })
+  add_Event_order_btn(product_grid);
   return product_grid;
 }
 
