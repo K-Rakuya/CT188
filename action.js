49ac7e7de726b0e7b857657b5b68fab6340ba85b
niodaaa <nghiakun234@gmail.com>
2026-03-22 21:49:46 +0700

    up 1.2

--- action.js
+++ action.js
@@ -25,12 +25,48 @@ function prevSlide() {
 document.querySelector(".next").onclick = nextSlide;
 document.querySelector(".prev").onclick = prevSlide;
 
-dots.forEach((dot, i) => {
-  dot.onclick = () => {
-    index = i;
-    showSlide(index);
-  };
+let selectedStars = 0;
+
+// chọn sao
+const stars = document.querySelectorAll("#starSelect span");
+
+stars.forEach((star) => {
+  star.addEventListener("click", () => {
+    selectedStars = parseInt(star.getAttribute("data-value"));
+
+    stars.forEach((s) => s.classList.remove("active"));
+    for (let i = 0; i < selectedStars; i++) {
+      stars[i].classList.add("active");
+    }
+  });
 });
 
-showSlide(index);
-setInterval(nextSlide, 3000);
+// thêm review
+function addReview() {
+  const name = document.getElementById("name").value.trim();
+  const comment = document.getElementById("comment").value.trim();
+
+  if (!name || !comment || selectedStars === 0) {
+    alert("Vui lòng nhập đầy đủ!");
+    return;
+  }
+
+  const reviewList = document.getElementById("reviewList");
+
+  const review = document.createElement("div");
+  review.classList.add("review");
+
+  review.innerHTML = `
+    <div>${"⭐".repeat(selectedStars)}</div>
+    <p>"${comment}"</p>
+    <small>- ${name}</small>
+  `;
+
+  reviewList.prepend(review);
+
+  // reset form
+  document.getElementById("name").value = "";
+  document.getElementById("comment").value = "";
+  selectedStars = 0;
+  stars.forEach((s) => s.classList.remove("active"));
+}
